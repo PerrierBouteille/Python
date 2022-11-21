adb48c94c1d650f086d418584b886612a17a5a2c
PerrierBottle <91777437+PerrierBouteille@users.noreply.github.com>
2022-11-21 11:54:21 +0100

    Update Tri_Fusion_NoLogs.py

--- TriFusion/Tri_Fusion_NoLogs.py
+++ TriFusion/Tri_Fusion_NoLogs.py
@@ -1,8 +1,7 @@
 def fusion(L1,L2):
-    n1,n2 = len(L1),len(L2)
-    L = [0]*(n1+n2)
+    L = [0]*(len(L1)+len(L2))
     i1,i2,i=0,0,0
-    while i1<n1 and i2<n2:
+    while i1<len(L1) and i2<len(L2):
         if L1[i1] < L2[i2]:
             L[i] = L1[i1]
             i1 += 1
@@ -10,11 +9,11 @@ def fusion(L1,L2):
             L[i] = L2[i2]
             i2 += 1
         i += 1
-    while i1<n1:
+    while i1<len(L1):
         L[i] = L1[i1]
         i1 += 1
         i += 1
-    while i2<n2: 
+    while i2<len(L2): 
         L[i] = L2[i2]
         i2 += 1
         i += 1
