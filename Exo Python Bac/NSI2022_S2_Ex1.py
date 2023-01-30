6ac88af1d24ff5f6683d31e4394dcd21d7ab1c75
PerrierBottle <91777437+PerrierBouteille@users.noreply.github.com>
2023-01-30 10:33:33 +0100

    Add files via upload

---
+++ Exo Python Bac/NSI2022_S2_Ex1.py
@@ -0,0 +1,13 @@
+﻿def moyenne(note):
+    H,B=0,0
+    for i in range(len(note)):
+        N = note[i][0]
+        C = note[i][1]
+
+
+        H = (N*C)+H
+        B += C
+
+    return H/B
+
+#moyenne([(15,2),(9,1),(12,3)])
