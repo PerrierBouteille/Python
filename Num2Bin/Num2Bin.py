8cffc827c4ec3a6a51c2a472f664416912a929b2
PerrierBottle <91777437+PerrierBouteille@users.noreply.github.com>
2023-03-02 08:58:55 +0100

    Add files via upload

---
+++ Num2Bin/Num2Bin.py
@@ -0,0 +1,6 @@
+﻿def bin(i,L):
+    L.append(i%2)
+    if(i<=0):
+        L.reverse()
+        return print(L)
+    bin(i//2,L)
---
+++ TriBulles/TriBulles.py
@@ -0,0 +1,7 @@
+﻿def tri(T):
+    for i in range(len(T)-1,0,-1):
+        for j in range(i):
+            if T[j] > T[j+1]:
+                temp,T[j] = T[j],T[j+1]
+                T[j+1] = temp
+    return T
